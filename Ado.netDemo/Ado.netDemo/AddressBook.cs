55660864de30fa15e95ab7032c211c9df010b2a1
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-05-27 10:24:12 +0530

    Add : Ado.net connection

---
+++ Ado.netDemo/Ado.netDemo/AddressBook.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ado.netDemo
+{
+    public class AddressBook
+    {
+        // ADo.Net -It is established conection between application and database.
+        //DataSource: It includes data servers,sql servers.
+        //ADO.net is used to CRUD(creating retriving,upadating,deleting)
+        public static string connectionString = @"Data Source=.;Initial Catalog=Address_Book_Service";
+        public void AddDataInDatabase(Contact contact)
+        {
+            SqlConnection sqlconnection=new SqlConnection(connectionString);
+            try
+            {
+                sqlconnection.Open();
+                SqlCommand sqlCommand = new SqlCommand("SPAddingData", sqlconnection);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.Parameters.AddWithValue("@FirstName",contact.FirstName);
+                sqlCommand.Parameters.AddWithValue("@LastName", contact.LastName);
+                sqlCommand.Parameters.AddWithValue("@Address", contact.Address);
+                sqlCommand.Parameters.AddWithValue("@City", contact.City);
+                sqlCommand.Parameters.AddWithValue("@State", contact.State);
+                sqlCommand.Parameters.AddWithValue("@PostalCode", contact.PostalCode);
+                sqlCommand.Parameters.AddWithValue("@PhoneNum", contact.PhoneNum);
+                sqlCommand.Parameters.AddWithValue("@Email", contact.Email);
+                int result=sqlCommand.ExecuteNonQuery();
+                sqlconnection.Close();
+                if(result >=1)
+                {
+                    Console.WriteLine("New Contact Added.");
+                }
+                else
+                {
+                    Console.WriteLine("error while adding data.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Ado.netDemo/Ado.netDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ado.netDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Contact contact = new Contact()
+            {
+                FirstName = "sara",
+                LastName = "siri",
+                Address = "abc",
+                City = "HYD",
+                State = "AP",
+                PostalCode = 23563,
+                PhoneNum = 25746322,
+                Email = "abcdgmail.com"
+            };
+            AddressBook book = new AddressBook();
+            book.AddDataInDatabase(contact);
+            Console.ReadLine();
+
+        }
+    }
+}
